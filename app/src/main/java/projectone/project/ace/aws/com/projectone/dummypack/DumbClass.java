40d47b66dff240e47e7462e8ee14a9b22d4ac997
RJ <roljohn.villanueva@awsys-i.com>
2015-11-24 11:07:07 +0800

    Hi commit

---
+++ app/src/main/java/projectone/project/ace/aws/com/projectone/dummypack/DumbClass.java
@@ -0,0 +1,8 @@
+package projectone.project.ace.aws.com.projectone.dummypack;
+
+/**
+ * Created by roljohn.villanueva on 11/24/2015.
+ */
+public class DumbClass {
+    private static String du = "Hello";
+}
