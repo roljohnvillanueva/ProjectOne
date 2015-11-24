dc1470d3c17afb6b458190da80243f55e9c9eef1
RJ <roljohn.villanueva@awsys-i.com>
2015-11-24 11:15:51 +0800

    erasing stuff

--- app/src/main/java/projectone/project/ace/aws/com/projectone/dummypack/DumbClass.java
+++
@@ -1,8 +0,0 @@
-package projectone.project.ace.aws.com.projectone.dummypack;
-
-/**
- * Created by roljohn.villanueva on 11/24/2015.
- */
-public class DumbClass {
-    private static String du = "Hello";
-}
